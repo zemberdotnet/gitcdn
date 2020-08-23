a10b5804b0b5c4dd249f4412b4f9b9efcac99f0f
zemberdotnet <matthew.zember@gmail.com>
2020-08-23 13:42:31 +0000

    n/a

---
+++ gmg3.js
@@ -0,0 +1,177 @@
+/*
+Copyright 2018–2020 Observable, Inc.
+
+Author: Mike Bostock
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+let adj = 10
+let padding = 10
+let x, y, xAxis, yAxis, line, path;
+let datas;
+let dot;
+
+
+margin = {top: 20, right: 20, bottom: 30, left: 30}
+
+
+chart = (data) => {
+
+
+
+data.dates = data.dates.map(d3.utcParse("%Y-%m-%d")); 
+datas = data;
+
+
+x = d3.scaleUtc()
+    .domain(d3.extent(data.dates))
+    .range([margin.left, width - margin.right]);
+
+y = d3.scaleLinear()
+    .domain([0, d3.max(data.series, d => d3.max(d.values))]).nice()
+    .range([height - margin.bottom, margin.top]);
+
+xAxis = g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+
+yAxis = g => g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y))
+    .call(g => g.select(".domain").remove())
+    .call(g => g.select(".tick:last-of-type text").clone()
+        .attr("x", 3)
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .text(data.y));
+
+line = d3.line()
+        .defined(d => !isNaN(d))
+        .x((d, i) => x(data.dates[i]))
+        .y(d => y(d))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const svg = d3.select(".content-wrapperc")
+        .append("svg")
+        .attr("viewBox", [0, 0, width, height])
+        .style("overflow", "visible");
+  
+    svg.append("g")
+        .call(xAxis);
+  
+    svg.append("g")
+        .call(yAxis);
+  
+    path = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+      .selectAll("path")
+      .data(data.series)
+      .join("path")
+        .style("mix-blend-mode", "multiply")
+        .attr("d", d => line(d.values));
+  
+    svg.call(hover, path);
+    console.log("finished chart");
+    return svg.node();
+
+
+    
+}
+
+
+
+function hover(svg, path) {
+  
+    if ("ontouchstart" in document) svg
+        .style("-webkit-tap-highlight-color", "transparent")
+        .on("touchmove", moved)
+        .on("touchstart", entered)
+        .on("touchend", left)
+    else svg
+        .on("mousemove", moved)
+        .on("mouseenter", entered)
+        .on("mouseleave", left);
+  
+    dot = svg.append("g")
+        .attr("display", "none");
+  
+    dot.append("circle")
+        .attr("r", 2.5);
+  
+    dot.append("text")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .attr("text-anchor", "middle")
+        .attr("y", -8);
+}
+
+function moved() {
+    d3.event.preventDefault();
+    const mouse = d3.mouse(this);
+    const xm = x.invert(mouse[0]);
+    const ym = y.invert(mouse[1]);
+    const i1 = d3.bisectLeft(datas.dates, xm, 1);
+    const i0 = i1 - 1;
+    const i = xm - datas.dates[i0] > datas.dates[i1] - xm ? i1 : i0;
+    const s = d3.least(datas.series, d => Math.abs(d.values[i] - ym));
+    path.attr("stroke", d => d === s ? null : "#ddd").filter(d => d === s).raise();
+    dot.attr("transform", `translate(${x(datas.dates[i])},${y(s.values[i])})`);
+    dot.select("text").text(s.values[i]);
+}
+
+function entered() {
+    path.style("mix-blend-mode", null).attr("stroke", "#ddd");
+    dot.attr("display", null);
+}
+
+function left() {
+    path.style("mix-blend-mode", "multiply").attr("stroke", null);
+    dot.attr("display", "none");
+}
+
+d3.json('https://raw.githubusercontent.com/zemberdotnet/gitcdn/master/output.json').then(chart);
+console.log("completed chart");
